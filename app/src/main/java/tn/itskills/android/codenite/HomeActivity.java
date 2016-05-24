144708367019d552821d58c5c9a8f4133b0343f5
adnen HAMDOUNI <adnen.hamdouni@gmail.com>
2016-05-24 20:24:33 +0100

    use firbase sigin, signout and create account

---
+++ app/src/main/java/tn/itskills/android/codenite/HomeActivity.java
@@ -0,0 +1,52 @@
+package tn.itskills.android.codenite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_signout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
---
+++ app/src/main/java/tn/itskills/android/codenite/models/User.java
@@ -0,0 +1,17 @@
+package tn.itskills.android.codenite.models;
+
+/**
+ * Created by adnenhamdouni on 24/05/2016.
+ */
+public class User {
+
+    public String username;
+
+    public User() {
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+}
